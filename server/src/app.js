3980d0dd811e79939104a724617573b2c4e2c12b
transkarpation <transkarpation@gmail.com>
2020-10-21 21:08:45 +0300

    add indexRouter to /api

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express');
 
 const { sequelize } = require('./db');
+const router = require('./routes');
 const { APP_PORT } = require('./config/app.config');
 
 const app = express();
 
 sequelize.authenticate().then(async () => {
   sequelize.sync({ force: true });
+
+  app.use('/api', router);
+
   app.listen(APP_PORT, () => {
     console.log(`Listening on ${APP_PORT}`);
   });
