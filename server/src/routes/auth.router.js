eb936cc3973aec70ee585d791670c6de760ed2fc
transkarpation <transkarpation@gmail.com>
2020-10-21 21:32:55 +0300

    npm i morgan

--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const loginMw = require('../middlewares/login.mw');
+const registerMw = require('../middlewares/register.mw');
 
 const authRouter = Router();
 
-authRouter.post('/login', (req, res) => res.send('/login'));
-authRouter.post('/register', (req, res) => res.send('/register'));
+authRouter.post('/login', loginMw, (req, res) => res.send('/login'));
+authRouter.post('/register', registerMw, (req, res) => res.send('/register'));
 
 module.exports = authRouter;
