5759e23c38b6aa513f48dc661ea1a71230adae88
transkarpation <transkarpation@gmail.com>
2020-10-21 21:24:55 +0300

    include auth.router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const authRouter = require('./auth.router');
 
 const indexRouter = Router();
 
 indexRouter.get('/', (req, res) => {
   res.send('hello');
 });
+indexRouter.use('/auth', authRouter);
 
 module.exports = indexRouter;
