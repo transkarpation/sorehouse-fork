6b04fce9204ce523453ab4852b12e5042d5669e2
transkarpation <transkarpation@gmail.com>
2020-10-21 20:58:07 +0300

    add User model

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const { APP_PORT } = require('./config/app.config');
 const app = express();
 
 sequelize.authenticate().then(async () => {
+  sequelize.sync({ force: true });
   app.listen(APP_PORT, () => {
     console.log(`Listening on ${APP_PORT}`);
   });
