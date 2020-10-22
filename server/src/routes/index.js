dbd24c02f35faf5543764e11e3d4b75203668027
transkarpation <transkarpation@gmail.com>
2020-10-22 06:20:18 +0300

    use unit.router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const authRouter = require('./auth.router');
 const goodsRouter = require('./goods.router');
+const unitRouter = require('./unit.router');
 
 const indexRouter = Router();
 
 indexRouter.use('/auth', authRouter);
 indexRouter.use('/goods', goodsRouter);
+indexRouter.use('/unit', unitRouter);
 
 module.exports = indexRouter;
