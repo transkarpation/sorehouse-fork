4a5ba717b54270eeea8449544c4633b183b3e0d5
transkarpation <transkarpation@gmail.com>
2020-10-22 05:15:59 +0300

    create goods.controller

---
+++ server/src/controllers/goods.controller.js
@@ -0,0 +1,11 @@
+const { Goods } = require('../db');
+
+module.exports = {
+  async get(req, res) {},
+
+  async store(req, res) {},
+
+  async update(req, res) {},
+
+  async delete(req, res) {},
+};
