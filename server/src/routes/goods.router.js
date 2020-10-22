ec6fce294d84fdaac6b88b7350aa911032e2f8ed
transkarpation <transkarpation@gmail.com>
2020-10-22 05:38:04 +0300

    include get for router

--- server/src/routes/goods.router.js
+++ server/src/routes/goods.router.js
@@ -4,6 +4,7 @@ const ctrl = require('../controllers/goods.controller');
 
 const router = Router();
 
+router.get('/', ctrl.get);
 router.post('/', goodsCreateValidators, ctrl.store);
 
 module.exports = router;
