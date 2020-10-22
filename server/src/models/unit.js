052a6a86128f44aa86d8f657570ecc00ac13ccc1
transkarpation <transkarpation@gmail.com>
2020-10-22 06:03:46 +0300

    create Unit model

---
+++ server/src/models/unit.js
@@ -0,0 +1,25 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Unit extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Unit.belongsToMany(models.Goods, { through: models.StoreGoods });
+    }
+  }
+  Unit.init(
+    {
+      shortName: DataTypes.STRING,
+      fullName: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Unit',
+    },
+  );
+  return Unit;
+};
