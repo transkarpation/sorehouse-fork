8e1a8423c6f7dbacfe99d016ebd6e7b0e9464d3c
transkarpation <transkarpation@gmail.com>
2020-10-22 05:19:24 +0300

    implements store method for goods.controller

--- server/src/controllers/goods.controller.js
+++ server/src/controllers/goods.controller.js
@@ -1,9 +1,22 @@
 const { Goods } = require('../db');
+const validate = require('../helpers/validationResult');
 
 module.exports = {
   async get(req, res) {},
 
-  async store(req, res) {},
+  async store(req, res) {
+    validate(req, res);
+
+    const { title, expirationTime } = req.body;
+    const result = await Goods.create({
+      title,
+      expirationTime,
+      GoodsCategoryId: req.body.categoryId,
+      UnitId: req.body.unitId,
+    });
+
+    return res.send(result.toJSON());
+  },
 
   async update(req, res) {},
 
