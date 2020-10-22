a17450efa225549e4453d1e8ba6cdf55e0e0f28f
transkarpation <transkarpation@gmail.com>
2020-10-22 06:24:59 +0300

    create Category model

---
+++ server/src/models/category.js
@@ -0,0 +1,21 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Category',
+    },
+  );
+  return Category;
+};
