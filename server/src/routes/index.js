485290bb740c35c8e640d21d85f56c74247fb314
transkarpation <transkarpation@gmail.com>
2020-10-22 06:41:21 +0300

    use categoryRouter

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const authRouter = require('./auth.router');
 const goodsRouter = require('./goods.router');
 const unitRouter = require('./unit.router');
+const categoryRouter = require('./category.router');
 
 const indexRouter = Router();
 
 indexRouter.use('/auth', authRouter);
 indexRouter.use('/goods', goodsRouter);
 indexRouter.use('/unit', unitRouter);
+indexRouter.use('/category', categoryRouter);
 
 module.exports = indexRouter;
