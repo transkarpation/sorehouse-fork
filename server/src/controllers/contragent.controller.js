d8250bab62323dc2a4e85206362c2b1a5d07cef5
transkarpation@gmail.com <transkarpation@gmail.com>
2020-10-22 08:55:09 +0300

    contragent.controller

---
+++ server/src/controllers/contragent.controller.js
@@ -0,0 +1,15 @@
+const validate = require('../helpers/validationResult');
+
+module.exports = {
+  async get(req, res) {;
+  },
+
+  async store(req, res) {
+  },
+
+  async update(req, res) {
+  },
+
+  async delete(req, res) {
+  },
+};
