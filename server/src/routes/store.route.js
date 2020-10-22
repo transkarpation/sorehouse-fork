9ff2ebf83f10652b23812a7259abfc31f0d7e94d
transkarpation <transkarpation@gmail.com>
2020-10-22 06:54:22 +0300

    store.router

---
+++ server/src/routes/store.route.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const {
+  storeCreateValidators,
+} = require('../middlewares/validations.mw');
+const ctrl = require('../controllers/store.controller');
+
+const router = Router();
+
+router.get('/', ctrl.get);
+router.post('/', storeCreateValidators, ctrl.store);
+router.put('/:id', storeCreateValidators, ctrl.update);
+router.delete('/:id', ctrl.delete);
+
+module.exports = router;
