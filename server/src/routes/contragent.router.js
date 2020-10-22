0e7b79a1baf0463005acaa97d48743b6893d8204
transkarpation@gmail.com <transkarpation@gmail.com>
2020-10-22 10:23:41 +0300

    create contragent validation

--- server/src/routes/contragent.router.js
+++ server/src/routes/contragent.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { contragentCreateValidators } = require('../middlewares/validations.mw');
 const ctrl = require('../controllers/contragent.controller');
 
 const router = Router();
 
 router.get('/', ctrl.get);
-router.post('/', ctrl.store);
+router.post('/', contragentCreateValidators, ctrl.store);
 router.put('/:id', ctrl.update);
 router.delete('/:id', ctrl.delete);
 
