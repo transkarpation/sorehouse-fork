4bfd3a7b280827e9bccd0d57f24ca8e7e6bd7f82
transkarpation <transkarpation@gmail.com>
2020-10-22 05:42:58 +0300

    include update

--- server/src/routes/goods.router.js
+++ server/src/routes/goods.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { goodsCreateValidators } = require('../middlewares/validations.mw');
+const { goodsCreateValidators, goodsUpdateValidators } = require('../middlewares/validations.mw');
 const ctrl = require('../controllers/goods.controller');
 
 const router = Router();
 
 router.get('/', ctrl.get);
 router.post('/', goodsCreateValidators, ctrl.store);
+router.post('/:id', goodsUpdateValidators, ctrl.update);
 
 module.exports = router;
