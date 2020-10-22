32624440c0176c9d5b7d922edb3073729c290eee
transkarpation <transkarpation@gmail.com>
2020-10-22 06:42:28 +0300

    create Store model

---
+++ server/src/models/store.js
@@ -0,0 +1,21 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Store extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  }
+  Store.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Store',
+    },
+  );
+  return Store;
+};
